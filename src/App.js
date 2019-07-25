f6e373b4b349310e8600b7e873b3cbe83f4e4bb6
andresshare <andresshare@gmail.com>
2019-07-24 22:36:07 -0500

    get data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
+
+    constructor(){
+        super();
+            this.state = {
+                monsters :[
+                    {
+                        name: 'Dracula',
+                        id: 'asd12'
+                    },
+                    {
+                        name: 'Godzilla',
+                        id: 'asd13'
+                    },
+                    {
+                        name: 'Kingkong',
+                        id: 'asd14'
+                    },
+
+                ]
+            }
+        }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response =>response.json())
+            .then(users =>this.setState({monsters:users}))
+    }
+
+    render() {
+      return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+          {
+              this.state.monsters.map(monster =>
+                      (
+                            <h1 key={monster.id} > {monster.name} </h1>)
+                      )
+          }
+
       </div>
     );
   }
